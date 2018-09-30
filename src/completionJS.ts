264c460dc261d21b73ec854e70a82db5b0ec88e7
chenshy39 <82263980@qq.com>
2018-09-30 22:25:16 +0800

    添加对refs的提示

--- src/completionJS.ts
+++ src/completionJS.ts
@@ -2,28 +2,47 @@ import * as vscode from 'vscode';
 import methods from './regularMethods';
 
 export class CompletionJS implements vscode.CompletionItemProvider {
-    private getFunction(content: string) {
-        const regExp = /(\w+)\(.*\){/g;
-        const result = regExp.exec(content);
-        console.log(result);
+    private getRefs(content: string) {
+        const completionItems:vscode.CompletionItem[] = [];
+        const regExp = /ref\s*=\s*\"*(\w+)\"*\s*/g;
+        
+        let result;
+        while((result = regExp.exec(content)) != null) {
+            console.log(result);
+            
+            const funcName = result[1];
+            
+            let completionItem = new vscode.CompletionItem(funcName, vscode.CompletionItemKind.Function);
+            completionItem.label = funcName;
+            completionItems.push(completionItem);
+        }
+
+        return completionItems;
         
     }
     
     public provideCompletionItems(document:vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.CompletionItem[] {
         let completionItems:vscode.CompletionItem[] = [];
-        // console.log(document.getText());
+        console.log('range', document.lineAt(position.line).text);
 
         const triggerCharacter = context.triggerCharacter;
         // 触发条件为.时
+        // console.log(context);
+        
         if (triggerCharacter === '.') {
-            this.getFunction(document.getText());
-            return completionItems;
+            const lineText = document.lineAt(position.line).text;
+            const isRefs = lineText.match(/\$refs/g);
+            if(isRefs) {
+                console.log('yes');
+                return this.getRefs(document.getText());
+            }
         }
+
         
         // 循环读取regular中的所有方法(snippet)并添加到CompletionItem中
         Object.keys(methods).map((method_name) => {
             let completionItem = new vscode.CompletionItem(method_name, methods[method_name].kind);
-            completionItem.kind = vscode.CompletionItemKind.Snippet;
+            // completionItem.kind = vscode.CompletionItemKind.Snippet;
 
             // 将数组改为字符串后作为insertText
             const snippet = methods[method_name].snippet.join('\n');
